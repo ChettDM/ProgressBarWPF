e39c8b49548edbe6cc18529f9c5201ce3a5a4833
Chett Michael <Chett.Michael@gmail.com>
2019-09-06 09:44:48 -0500

    Changed the entire process

--- PrgressBarWPF/MainWindow.xaml.cs
+++ PrgressBarWPF/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using PrgressBarWPF.Annotations;
 
 namespace PrgressBarWPF
 {
@@ -23,50 +25,64 @@ namespace PrgressBarWPF
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-        private readonly BackgroundWorker _backgroundWorker = new BackgroundWorker();
+    { 
+        //private readonly BackgroundWorker _backgroundWorker = new BackgroundWorker();
 
         // Super important that the percentage is stored up
         // here as the casting to an int, will keep the
         // entire thing at 0% for extremely large processes.
-        private double _progressPercentage;
+        public PBar ProgressBar = new PBar();
 
         public MainWindow()
         {
             InitializeComponent();
-            Progress.Value = 0;
 
-            _backgroundWorker.WorkerReportsProgress = true;
-            _backgroundWorker.WorkerSupportsCancellation = true;
-            _backgroundWorker.ProgressChanged += (sender, args) => Progress.Value = args.ProgressPercentage; 
-            _backgroundWorker.RunWorkerCompleted += (sender, args) => Progress.Value = 100;
-            _backgroundWorker.DoWork += (sender, args) =>
-            {
-                for (var i = 0; i < 100; i++)
-                {
-                    if (((BackgroundWorker) sender).CancellationPending)
-                    {
-                        args.Cancel = true;
-                        break;
-                    }
-                    
-                    _backgroundWorker.ReportProgress((int)(_progressPercentage =+ i));
-                    Thread.Sleep(500);
-                }
-            };
+
+            //_backgroundWorker.WorkerReportsProgress = true;
+            //_backgroundWorker.WorkerSupportsCancellation = true;
+            //_backgroundWorker.ProgressChanged += (sender, args) => Progress.Value = args.ProgressPercentage; 
+            //_backgroundWorker.RunWorkerCompleted += (sender, args) => Progress.Value = 100;
+            //_backgroundWorker.DoWork += (sender, args) =>
+            //{
+            //    for (var i = 0; i < 100; i++)
+            //    {
+            //        if (((BackgroundWorker) sender).CancellationPending)
+            //        {
+            //            args.Cancel = true;
+            //            break;
+            //        }
+
+            //        _backgroundWorker.ReportProgress((int)(_progressPercentage =+ i));
+            //        Thread.Sleep(500);
+            //    }
+            //};
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            _backgroundWorker.RunWorkerAsync();
+            Task.Run(() => 
+            {
+                for (var i = 0; i < 100; i++)
+                { 
+                    SetProgressBarValue(i);
+                    Thread.Sleep(500);
+                }
+            });
         }
 
 
         private void ButtonBase2_OnClick(object sender, RoutedEventArgs e)
         {
-            _backgroundWorker.CancelAsync(); // <= Doesn't work
+            //_backgroundWorker.CancelAsync(); // <= Doesn't work
         }
 
-        
+        private void SetProgressBarValue(int percentage)
+        {
+            Dispatcher?.Invoke(() =>
+            {
+                Progress.Value = percentage;
+                //ProgressBar.ProgressPercentage = i;
+            });
+        }
     }
 }
---
+++ PrgressBarWPF/PBar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PrgressBarWPF.Annotations;
+
+namespace PrgressBarWPF
+{
+    public class PBar : INotifyPropertyChanged
+    {
+        public double _progressPercentage = 0;
+        public double ProgressPercentage
+        {
+            get => _progressPercentage;
+            set
+            {
+                if (Math.Abs(value - _progressPercentage) < -1) return;
+                _progressPercentage = value;
+                OnPropertyChanged(nameof(ProgressPercentage));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
